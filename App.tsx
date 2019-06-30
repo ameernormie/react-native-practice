d4b95c3575c2130e472d1992a2e0a0cd123902c2
Ameer Hamza <12bscsahamza@seecs.edu.pk>
2019-06-30 11:52:09 +0500

    feat: basic formik implementation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,29 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
- * 
+ *
  * Generated with the TypeScript template
  * https://github.com/emin93/react-native-template-typescript
- * 
+ *
  * @format
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button
+} from "react-native";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 interface Props {}
@@ -23,9 +31,48 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <View
+          style={{
+            alignSelf: "stretch",
+            minHeight: 200,
+            marginHorizontal: 20
+          }}
+        >
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            onSubmit={values => console.log(values)}
+          >
+            {props => (
+              <View>
+                <TextInput
+                  onChangeText={props.handleChange("email")}
+                  onBlur={props.handleBlur("email")}
+                  value={props.values.email}
+                  style={{
+                    backgroundColor: "grey",
+                    height: 50,
+                    marginBottom: 5,
+                    paddingHorizontal: 10,
+                    borderRadius: 5
+                  }}
+                />
+                <TextInput
+                  onChangeText={props.handleChange("password")}
+                  onBlur={props.handleBlur("password")}
+                  value={props.values.password}
+                  style={{
+                    backgroundColor: "grey",
+                    height: 50,
+                    marginBottom: 5,
+                    paddingHorizontal: 10,
+                    borderRadius: 5
+                  }}
+                />
+                <Button onPress={props.handleSubmit} title="Submit" />
+              </View>
+            )}
+          </Formik>
+        </View>
       </View>
     );
   }
@@ -34,18 +81,18 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
