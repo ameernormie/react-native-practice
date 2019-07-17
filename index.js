b44bae30609e827a7a071f9f81314ef74d2a7c5c
Ameer Hamza <12bscsahamza@seecs.edu.pk>
2019-07-17 14:37:52 +0500

    refactor: change root component to register react navigation

--- index.js
+++ index.js
@@ -2,8 +2,19 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { Navigation } from "react-native-navigation";
+import { name as appName } from "./app.json";
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: "navigation.playground.WelcomeScreen"
+      }
+    }
+  });
+});
